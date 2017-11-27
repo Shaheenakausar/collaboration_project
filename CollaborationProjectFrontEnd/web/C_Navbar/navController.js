005529bdd970debd1340efd136fdf179df9a11a9
ShaheenaKausar <lalakausar@gmail.com>
2017-11-27 23:20:38 +0530

    updated
    updated

--- CollaborationProjectFrontEnd/web/C_Navbar/navController.js
+++
@@ -1,6 +0,0 @@
-myApp.controller("navController",function($scope,$location){
-	console.log($location.path());
-	$scope.isActive = function(destination){
-		return destination === $location.path();
-	};
-});
